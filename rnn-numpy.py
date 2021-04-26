5193ae4a5e1c5707538cc1c8400f534b1767b215
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2021-04-26 13:21:58 -0400

    rnn numpy code started

--- rnn-numpy.py
+++ rnn-numpy.py
@@ -3,7 +3,7 @@ import numpy as np
 ## Creating our dataset
 with open('sherlock_homes.txt', 'r') as file:
     text = file.read().lower()
-# print('text length', len(text))
+print('text length', len(text))
 # print('text length', text)
 chars = sorted(list(set(text))) ## get only unique characters
 # print('total chars: ', len(chars))
@@ -24,7 +24,26 @@ for i in range(0, len(text) - maxlen, step):
 # print(f'im sentence {sentences[0:3]}')
 # print(f'im text chars {text[40:43]}')
 # print(f'im nextchars {next_chars[0:3]}')
+#x is: For each sentence of length maxLen(40), make an array of unique chars
+# and put 1 if the character is present in the sentence
+print(len(sentences))
 x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+# y is next character in the sentence so for each sentence one char
+# array containing the next element
+
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+# write output to file to see
+import sys
+sys.stdout = open('file', 'w')
+print(x[0])
+sys.stdout.close()
+
 
 
 
