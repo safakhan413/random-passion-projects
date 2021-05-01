5fe0c83eb84fe0e5e261de70d548b53eb9119655
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2021-05-01 01:31:54 -0400

    commit rename

--- decision tree from scratch python/predict_wine_type.py
+++ decision tree from scratch python/predict_wine_type.py
@@ -5,10 +5,10 @@ import pandas
 import time
 from decisionTree import trainTestSplit, buildDecisionTree, decisionTreePredictions, calculateAccuracy
 
-dataFrame = pandas.read_csv("WINE_DATA.csv")
+dataFrame = pandas.read_csv("../datasets/WINE_DATA.csv")
 # dataFrame = dataFrame.drop("id", axis = 1)
-dataFrameTrain = dataFrame[dataFrame.columns.tolist()[1: ] + dataFrame.columns.tolist()[0: 1]]
-# dataFrameTrain, dataFrameTest = trainTestSplit(dataFrame, testSize = )
+# dataFrameTrain = dataFrame[dataFrame.columns.tolist()[1: ] + dataFrame.columns.tolist()[0: 1]]
+dataFrameTrain, dataFrameValidation = trainTestSplit(dataFrame, testSize = 0.25)
 
 print("Decision Tree - Predict Wine dataset")
 
@@ -40,9 +40,9 @@ def Train():
         decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
         buildingTime = time.time() - startTime
 
-def solution(WINE_DATA, test_user):
+def solution(test_user):
     # Your code goes here
-
+    decisio
     # while accuracyTrain < 100:
     #     startTime = time.time()
     #     decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
@@ -68,9 +68,10 @@ dataFrameTest = [
     [2, 12.37, .94, 1.36, 10.6, 88, 1.98, .57, .28, .42, 1.95, 1.05, 1.82, 1.0, 520]
 ]
 # solution(WINE_DATA, TEST_USER[1:])
-
+decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
 for TEST_USER in TEST_USERS:
-    WINE_TYPE = solution(WINE_DATA, TEST_USER[1:],)
+    WINE_TYPE = solution( TEST_USER[1:])
+
     if WINE_TYPE == TEST_USER[0]:
         print(True)
     else:
