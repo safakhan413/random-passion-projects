6e91f356758be97a656f0a3e9e170a084f609201
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2021-05-02 23:05:36 -0400

    chenges to wine data

--- decision tree from scratch python/predict_wine_type.py
+++ decision tree from scratch python/predict_wine_type.py
@@ -11,9 +11,25 @@ dataFrame = pandas.read_csv("../datasets/WINE_DATA.csv")
 dataFrameTrain, dataFrameValidation = trainTestSplit(dataFrame, testSize = 0.25)
 
 print("Decision Tree - Predict Wine dataset")
-
 i = 1
 accuracyTrain = 0
+while accuracyTrain < 100:
+    startTime = time.time()
+    decisionTree = buildDecisionTree(dataFrameTrain, maxDepth=i)
+    buildingTime = time.time() - startTime
+    # decisionTreeTestResults = decisionTreePredictions(dataFrameTest, decisionTree)
+    # accuracyTest = calculateAccuracy(decisionTreeTestResults, dataFrameTest.iloc[:, -1]) * 100
+    decisionTreeTrainResults = decisionTreePredictions(dataFrameTrain, decisionTree)
+    accuracyTrain = calculateAccuracy(decisionTreeTrainResults, dataFrameTrain.iloc[:, 0]) * 100
+    print("maxDepth = {}: ".format(i), end="")
+    # print("accTest = {0:.2f}%, ".format(accuracyTest), end = "")
+    print("accTrain = {0:.2f}%, ".format(accuracyTrain), end="")
+    # print("buildTime = {0:.2f}s".format(buildingTime), end = "\n")
+    i += 1
+
+
+# i = 1
+# accuracyTrain = 0
 # while accuracyTrain < 100:
 #     startTime = time.time()
 #     decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
@@ -34,45 +50,48 @@ TEST_USERS = [
     [2, 12.37, .94, 1.36, 10.6, 88, 1.98, .57, .28, .42, 1.95, 1.05, 1.82, 1.0, 520]
 ]
 
-def Train():
-    while accuracyTrain < 100:
-        startTime = time.time()
-        decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
-        buildingTime = time.time() - startTime
+# def Train():
+#     while accuracyTrain < 100:
+#         startTime = time.time()
+#         decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
+#         buildingTime = time.time() - startTime
 
-def solution(test_user):
-    # Your code goes here
-    decisio
-    # while accuracyTrain < 100:
-    #     startTime = time.time()
-    #     decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
-    #     buildingTime = time.time() - startTime
-    #     decisionTreeTestResults = decisionTreePredictions(dataFrameTest, decisionTree)
-    #     accuracyTest = calculateAccuracy(decisionTreeTestResults, dataFrameTest.iloc[:, -1]) * 100
-    #     decisionTreeTrainResults = decisionTreePredictions(dataFrameTrain, decisionTree)
-    #     accuracyTrain = calculateAccuracy(decisionTreeTrainResults, dataFrameTrain.iloc[:, -1]) * 100
-    #     print("maxDepth = {}: ".format(i), end = "")
-    #     print("accTest = {0:.2f}%, ".format(accuracyTest), end = "")
-    #     print("accTrain = {0:.2f}%, ".format(accuracyTrain), end = "")
-    #     print("buildTime = {0:.2f}s".format(buildingTime), end = "\n")
-    #     i += 1
-
-    # X = [3, 12.96, 3.45, 2.35, 18.5, 106, 1.39, .7, .4, .94, 5.28, .68, 1.75, 1.0, 675],
-
-    return WINE_TYPE
-
-dataFrameTest = [
-    [3, 12.96, 3.45, 2.35, 18.5, 106, 1.39, .7, .4, .94, 5.28, .68, 1.75, 1.0, 675],
-    [3, 13.17, 2.59, 2.37, 20, 120, 1.65, .68, .53, 1.46, 9.3, .6, 1.62, 1.0, 840],
-    [1, 13.16, 2.36, 2.67, 18.6, 101, 2.8, 3.24, .3, 2.81, 5.68, 1.03, 3.17, 1.0, 1185],
-    [2, 12.37, .94, 1.36, 10.6, 88, 1.98, .57, .28, .42, 1.95, 1.05, 1.82, 1.0, 520]
-]
-# solution(WINE_DATA, TEST_USER[1:])
-decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
-for TEST_USER in TEST_USERS:
-    WINE_TYPE = solution( TEST_USER[1:])
-
-    if WINE_TYPE == TEST_USER[0]:
-        print(True)
-    else:
-        print(False)
+# def solution(test_user):
+#     # Your code goes here
+#     # decisio
+#     i = 1
+#     accuracyTrain = 0
+#     # print(dataFrameTrain.iloc[:,0])
+#     while accuracyTrain < 100:
+#         startTime = time.time()
+#         decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
+#         buildingTime = time.time() - startTime
+#         # decisionTreeTestResults = decisionTreePredictions(dataFrameTest, decisionTree)
+#         # accuracyTest = calculateAccuracy(decisionTreeTestResults, dataFrameTest.iloc[:, -1]) * 100
+#         decisionTreeTrainResults = decisionTreePredictions(dataFrameTrain, decisionTree)
+#         accuracyTrain = calculateAccuracy(decisionTreeTrainResults, dataFrameTrain.iloc[:,0]) * 100
+#         print("maxDepth = {}: ".format(i), end = "")
+#         # print("accTest = {0:.2f}%, ".format(accuracyTest), end = "")
+#         print("accTrain = {0:.2f}%, ".format(accuracyTrain), end = "")
+#         # print("buildTime = {0:.2f}s".format(buildingTime), end = "\n")
+#         i += 1
+#
+#     # X = [3, 12.96, 3.45, 2.35, 18.5, 106, 1.39, .7, .4, .94, 5.28, .68, 1.75, 1.0, 675],
+#     return 0
+#     # return WINE_TYPE
+#
+# dataFrameTest = [
+#     [3, 12.96, 3.45, 2.35, 18.5, 106, 1.39, .7, .4, .94, 5.28, .68, 1.75, 1.0, 675],
+#     [3, 13.17, 2.59, 2.37, 20, 120, 1.65, .68, .53, 1.46, 9.3, .6, 1.62, 1.0, 840],
+#     [1, 13.16, 2.36, 2.67, 18.6, 101, 2.8, 3.24, .3, 2.81, 5.68, 1.03, 3.17, 1.0, 1185],
+#     [2, 12.37, .94, 1.36, 10.6, 88, 1.98, .57, .28, .42, 1.95, 1.05, 1.82, 1.0, 520]
+# ]
+# # solution(WINE_DATA, TEST_USER[1:])
+# # decisionTree = buildDecisionTree(dataFrameTrain, maxDepth = i)
+# for TEST_USER in TEST_USERS:
+#     WINE_TYPE = solution( TEST_USER[1:])
+#
+#     if WINE_TYPE == TEST_USER[0]:
+#         print(True)
+#     else:
+#         print(False)
