22440ab2ca594d505a9863dc64531acfa6a21d18
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-07-16 12:55:06 -0400

    main.py changes

---
+++ cnn-pytorch/main.py
@@ -0,0 +1,137 @@
+# https://www.analyticsvidhya.com/blog/2019/10/building-image-classification-models-cnn-pytorch/
+
+
+############################### importing the libraries ###########################
+import pandas as pd
+import numpy as np
+
+# for reading and displaying images
+from skimage.io import imread
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+# for creating validation set
+from sklearn.model_selection import train_test_split
+
+# for evaluating the model
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
+
+# PyTorch libraries and modules
+import torch
+from torch.autograd import Variable
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.optim import Adam, SGD
+
+######################## Load the dataset ################################
+
+# loading dataset
+train = pd.read_csv('train_LbELtWX/train.csv')
+test = pd.read_csv('test_ScVgIM0/test.csv')
+
+sample_submission = pd.read_csv('sample_submission_I5njJSF.csv')
+
+train.head()
+
+############ loading training images ############################
+train_img = []
+print('######## Converting train images to numpy array with float values')
+for img_name in tqdm(train['id']):
+    # defining the image path
+    image_path = 'train_LbELtWX/train/' + str(img_name) + '.png'
+    # reading the image
+    img = imread(image_path, as_gray=True)
+    # normalizing the pixel values
+    img /= 255.0
+    # converting the type of pixel to float 32
+    img = img.astype('float32')
+    # appending the image into the list
+    train_img.append(img)
+
+# converting the list to numpy array
+train_x = np.array(train_img)
+# defining the target
+train_y = train['label'].values
+tp = train['id'].values
+print(f"\nI'm the id values {train_y}###\n")
+train_x.shape
+
+################################## visualizing images ################################
+i = 0
+plt.figure(figsize=(10,10))
+plt.subplot(221), plt.imshow(train_x[i], cmap='gray')
+plt.subplot(222), plt.imshow(train_x[i+25], cmap='gray')
+plt.subplot(223), plt.imshow(train_x[i+50], cmap='gray')
+plt.subplot(224), plt.imshow(train_x[i+75], cmap='gray')
+
+
+################### splitting data into validation and train ###########################
+# create validation set
+train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size = 0.1)
+(train_x.shape, train_y.shape), (val_x.shape, val_y.shape)
+
+# converting training images into torch format
+train_x = train_x.reshape(54000, 1, 28, 28)
+train_x  = torch.from_numpy(train_x)
+
+# converting the target into torch format
+train_y = train_y.astype(int);
+train_y = torch.from_numpy(train_y)
+
+# shape of training data
+train_x.shape, train_y.shape
+
+
+# converting validation images into torch format
+val_x = val_x.reshape(6000, 1, 28, 28)
+val_x  = torch.from_numpy(val_x)
+
+# converting the target into torch format
+val_y = val_y.astype(int);
+val_y = torch.from_numpy(val_y)
+
+# shape of validation data
+val_x.shape, val_y.shape
+
+#################### CNN Architecture for forward pass ##############################
+class Net(Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.cnn_layers = Sequential(
+            # Defining a 2D convolution layer
+            Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(4),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+            # Defining another 2D convolution layer
+            Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(4),
+            ReLU(inplace=True),
+            MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.linear_layers = Sequential(
+            Linear(4 * 7 * 7, 10)
+        )
+
+    # Defining the forward pass
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        return x
+
+
+# defining the model
+model = Net()
+# defining the optimizer
+optimizer = Adam(model.parameters(), lr=0.07)
+# defining the loss function
+criterion = CrossEntropyLoss()
+# checking if GPU is available
+if torch.cuda.is_available():
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+print(model)
