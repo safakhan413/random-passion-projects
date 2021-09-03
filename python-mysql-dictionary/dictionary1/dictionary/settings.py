2a83a46feca7eeec538f60b033b7e7a1cec8df27
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-09-02 22:51:16 -0400

    prcofile, runtime.txt and requirements./txt created

--- python-mysql-dictionary/dictionary1/dictionary/settings.py
+++ python-mysql-dictionary/dictionary1/dictionary/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&e5z@dhmny9(zaf8us3%o(0vshnz@mdp)p6bqd*+cym^lkhqmo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dictionary1safa.herokuapp.com', '127.0.0.1/']
 
 
 # Application definition
